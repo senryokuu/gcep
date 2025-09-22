c4d215eb5f3d3ba6f80788802740e9c3c9ed5f75
dull-4h4n <dokuryu.0222@gmail.com>
2025-09-22 19:23:48 +0800

    fixed profile page's responsiveness

--- src/data/userCreds.ts
+++ src/data/userCreds.ts
@@ -1,8 +1,8 @@
 export const usercreds = [
 {
       pfp: "assets/pfp/backpain.gif",
-      username: "admin",
-      name: "Admin",
+      username: "admin_username",
+      name: "admin_name",
       studentid: "99999999",
       department: "BSCS",
 },
