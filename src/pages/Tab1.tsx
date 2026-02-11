dcde36cfc17dc5bfeaacc81a8b77f43a8b1cebf3
dull-4h4n <dokuryu.0222@gmail.com>
2026-02-11 22:20:06 +0800

    fixed alot of errors in home page and restored login page before bad commit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,75 +1,151 @@
-import { IonIcon, IonFab, IonFabButton, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
-import { home } from 'ionicons/icons';
-import './Tabs.css';
-import { events } from '../data/eventsData';
-import React from 'react';
-
-export const getTagColorClass = (tag: string) => {
+import {
+	IonIcon,
+	IonFab,
+	IonFabButton,
+	IonButton,
+	IonCard,
+	IonCardContent,
+	IonCardHeader,
+	IonCardSubtitle,
+	IonCardTitle,
+	IonCol,
+	IonContent,
+	IonGrid,
+	IonHeader,
+	IonPage,
+	IonRow,
+	IonTitle,
+	IonToolbar
+  } from '@ionic/react';
+  
+  import { home } from 'ionicons/icons';
+  import './Tabs.css';
+  import React, { useEffect, useState } from 'react';
+  import { db } from "../firebaseConfig";
+  import { collection, getDocs } from "firebase/firestore";
+  
+  export const getTagColorClass = (tag: string) => {
 	switch (tag) {
-		case "Gordon College":
-			return "gc-tag";
-		case "BSCS":
-			return "bscs-tag";
-		case "CBA":
-			return "cba-tag";
-		default:
-			return "";
+	  case "Gordon College":
+		return "gc-tag";
+	  case "BSCS":
+		return "bscs-tag";
+	  case "CBA":
+		return "cba-tag";
+	  default:
+		return "";
 	}
-};
-
-const Tab1: React.FC = () => {
-	const [allEvents, setAllEvents] = React.useState(events);
-
-	React.useEffect(() => {
-		const storedEvent = localStorage.getItem('newEvent');
-		if (storedEvent) {
-			const newEvent = JSON.parse(storedEvent);
-			setAllEvents([newEvent, ...events]); // Add new event to the top of the list
-			localStorage.removeItem('newEvent'); // Cleanly remove the item from storage
+  };
+  
+  const Tab1: React.FC = () => {
+  
+	const [allEvents, setAllEvents] = useState<any[]>([]);
+  
+	useEffect(() => {
+	  const fetchEvents = async () => {
+		try {
+		  const querySnapshot = await getDocs(collection(db, "events"));
+  
+		  const eventsData = querySnapshot.docs.map((document) => {
+			return {
+			  id: document.id,
+			  ...document.data()
+			};
+		  });
+  
+		  eventsData.sort((a: any, b: any) =>
+			b?.cAt?.toMillis?.() - a?.cAt?.toMillis?.()
+		  );
+  
+		  setAllEvents(eventsData);
+  
+		} catch (error) {
+		  console.error("Error fetching events:", error);
 		}
-	}, []); // Run this effect only once when the component mounts
-
+	  };
+  
+	  fetchEvents();
+	}, []);
+  
 	return (
-		<IonPage>
-			<IonHeader>
-				<IonToolbar>
-					<div style={{ display: 'flex', alignItems: 'center' }}>
-						<img src="assets/favicon.png" alt="logo" style={{ width: '40px', height: '40px', marginBottom: '10px', marginLeft: '20px' }} />
-						<IonTitle style={{ marginTop: '5px' }}>Home</IonTitle>
-					</div>
-				</IonToolbar>
-			</IonHeader>
-
-			<IonContent fullscreen>
-				<IonGrid>
-					<IonRow>
-						<IonCol>
-							{allEvents.map((event, index) => (
-								<IonCard key={index} className="post">
-									<img src={event.image} alt={event.title} style={{ height: '300px', width: '100%', objectFit: 'cover' }} />
-									<IonCardHeader>
-										<IonCardTitle>{event.title}</IonCardTitle>
-										<IonCardSubtitle>
-											{event.filter1 && (<span className={`tag ${getTagColorClass(event.filter1)}`}>{event.filter1}</span>)}
-											{event.filter2 && (<span className={`tag ${getTagColorClass(event.filter2)}`}>{event.filter2}</span>)}
-											<br /><br /> {event.time} - {event.date}
-										</IonCardSubtitle>
-									</IonCardHeader>
-									<IonCardContent className="events-description">{event.description}</IonCardContent>
-									<IonButton fill="clear">Sign up</IonButton>
-								</IonCard>
-							))}
-						</IonCol>
-					</IonRow>
-				</IonGrid>
-					<IonFab vertical="bottom" horizontal="center" className="home-fab">
-						<IonFabButton routerLink="/home" className="home-fab-button">
-							<IonIcon icon={home} />
-						</IonFabButton>
-					</IonFab>
-			</IonContent>
-		</IonPage>
+	  <IonPage>
+		<IonHeader>
+		  <IonToolbar>
+			<div style={{ display: 'flex', alignItems: 'center' }}>
+			  <img
+				src="assets/favicon.png"
+				alt="logo"
+				style={{ width: '40px', height: '40px', marginBottom: '10px', marginLeft: '20px' }}
+			  />
+			  <IonTitle style={{ marginTop: '5px' }}>Home</IonTitle>
+			</div>
+		  </IonToolbar>
+		</IonHeader>
+  
+		<IonContent fullscreen>
+		  <IonGrid>
+			<IonRow>
+			  <IonCol>
+  
+				{allEvents.length === 0 && (
+				  <p style={{ textAlign: 'center', marginTop: '50px' }}>
+					Loading events...
+				  </p>
+				)}
+  
+				{allEvents.map((event: any, index: number) => (
+				  <IonCard key={event.id || index} className="post">
+  
+					{event.image && (
+					  <img
+						src={event.image}
+						alt={event.title}
+						style={{ height: '300px', width: '100%', objectFit: 'cover' }}
+					  />
+					)}
+  
+					<IonCardHeader>
+					  <IonCardTitle>{event.title}</IonCardTitle>
+					  <IonCardSubtitle>
+  
+						{event.tags && event.tags.map((tag: string, idx: number) => (
+						  <span key={idx} className={`tag ${getTagColorClass(tag)}`}>
+							{tag}
+						  </span>
+						))}
+  
+						<br /><br />
+  
+						{event.timedate &&
+						  new Date(event.timedate.seconds * 1000).toLocaleString()
+						}
+  
+					  </IonCardSubtitle>
+					</IonCardHeader>
+  
+					<IonCardContent className="events-description">
+					  {event.desc}
+					</IonCardContent>
+  
+					<IonButton fill="clear">Sign up</IonButton>
+  
+				  </IonCard>
+				))}
+  
+			  </IonCol>
+			</IonRow>
+		  </IonGrid>
+  
+		  <IonFab vertical="bottom" horizontal="center" className="home-fab">
+			<IonFabButton routerLink="/home" className="home-fab-button">
+			  <IonIcon icon={home} />
+			</IonFabButton>
+		  </IonFab>
+  
+		</IonContent>
+	  </IonPage>
 	);
-};
-
-export default Tab1;
+  };
+  
+  export default Tab1;
+  
